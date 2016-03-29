4f150d048387eab65a6854067e21457a50ecfa9b
qianxing.cqx <qianxing.cqx@alibaba-inc.com>
2016-03-29 20:20:05 +0800

    delete home

--- ChedifierHome/PluginEngine/src/com/chedifier/plugin/Plugin.java
+++
@@ -1,8 +0,0 @@
-package com.chedifier.plugin;
-
-import com.chedifier.plugin.base.IPluginDescriptor;
-
-public class Plugin {
-	public IPluginDescriptor entry;
-	public PluginApplication application;
-}
